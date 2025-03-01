9a5769bffa0b3d9192ab8751e2f360d833adc803
Oscar Alfonso Guerrero <oaguerrero.95@gmail.com>
2025-03-01 00:45:47 -0600

    v1.0.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## v1.0.6
+
+- Changed the plugin to be a constant instead of a function.
+- Removed the fresh dependency from the project.
+- Removed the `deno.lock` file.
+
 ## v1.0.5
 
 - Added an explicit export type to the plugin.
--- mod.ts
+++ mod.ts
@@ -10,7 +10,6 @@
  *
  * @module lunchbox-plugin
  */
-import type { Plugin } from "$fresh/server.ts";
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 /**
@@ -27,79 +26,77 @@ const url =
  * @returns {Plugin}
  *  A plugin for the Deno compiler that preloads the Figtree and FiraCode fonts.
  */
-export function lunchbox(): Plugin {
-  return {
-    name: "lunchbox-fonts",
-    render: (ctx: {
-      render: () => {
-        htmlText: string;
-        requiresHydration: boolean;
-      };
-    }) => {
-      ctx.render();
-      return {
-        links: [
-          // Preload Figtree fonts
-          {
-            href: url + "/Figtree/Figtree-700.woff2",
-            as: "font",
-            type: "font/woff2",
-            crossorigin: "anonymous",
-          },
-          {
-            href: url + "/Figtree/Figtree-Italic.woff2",
-            as: "font",
-            type: "font/woff2",
-            crossorigin: "anonymous",
-          },
-          {
-            href: url + "/Figtree/Figtree-Regular.woff2",
-            as: "font",
-            type: "font/woff2",
-            crossorigin: "anonymous",
-          },
+export const lunchbox = {
+  name: "lunchbox-fonts",
+  render: (ctx: {
+    render: () => {
+      htmlText: string;
+      requiresHydration: boolean;
+    };
+  }) => {
+    ctx.render();
+    return {
+      links: [
+        // Preload Figtree fonts
+        {
+          href: url + "/Figtree/Figtree-700.woff2",
+          as: "font",
+          type: "font/woff2",
+          crossorigin: "anonymous",
+        },
+        {
+          href: url + "/Figtree/Figtree-Italic.woff2",
+          as: "font",
+          type: "font/woff2",
+          crossorigin: "anonymous",
+        },
+        {
+          href: url + "/Figtree/Figtree-Regular.woff2",
+          as: "font",
+          type: "font/woff2",
+          crossorigin: "anonymous",
+        },
 
-          // Preload FiraCode fonts
-          {
-            href: url + "/FiraCode/FiraCode-Bold.woff2",
-            as: "font",
-            type: "font/woff2",
-            crossorigin: "anonymous",
-          },
-          {
-            href: url + "/FiraCode/FiraCode-Regular.woff2",
-            as: "font",
-            type: "font/woff2",
-            crossorigin: "anonymous",
-          },
+        // Preload FiraCode fonts
+        {
+          href: url + "/FiraCode/FiraCode-Bold.woff2",
+          as: "font",
+          type: "font/woff2",
+          crossorigin: "anonymous",
+        },
+        {
+          href: url + "/FiraCode/FiraCode-Regular.woff2",
+          as: "font",
+          type: "font/woff2",
+          crossorigin: "anonymous",
+        },
 
-          // Preload Libre Caslon Text fonts
-          {
-            href: url + "/LibreCaslonText/Libre-Caslon-Text-700.woff2",
-            as: "font",
-            type: "font/woff2",
-            crossorigin: "anonymous",
-          },
-          {
-            href: url + "/LibreCaslonText/Libre-Caslon-Text-Italic.woff2",
-            as: "font",
-            type: "font/woff2",
-            crossorigin: "anonymous",
-          },
-          {
-            href: url + "/LibreCaslonText/Libre-Caslon-Text-Regular.woff2",
-            as: "font",
-            type: "font/woff2",
-            crossorigin: "anonymous",
-          },
+        // Preload Libre Caslon Text fonts
+        {
+          href: url + "/LibreCaslonText/Libre-Caslon-Text-700.woff2",
+          as: "font",
+          type: "font/woff2",
+          crossorigin: "anonymous",
+        },
+        {
+          href: url + "/LibreCaslonText/Libre-Caslon-Text-Italic.woff2",
+          as: "font",
+          type: "font/woff2",
+          crossorigin: "anonymous",
+        },
+        {
+          href: url + "/LibreCaslonText/Libre-Caslon-Text-Regular.woff2",
+          as: "font",
+          type: "font/woff2",
+          crossorigin: "anonymous",
+        },
 
-          // Load the fonts.css for font definitions
-          {
-            rel: "stylesheet",
-            href: url + "/fonts.css",
-          },
-        ],
-      };
-    },
-  };
-}
+        // Load the fonts.css for font definitions
+        {
+          rel: "stylesheet",
+          href: url + "/fonts.css",
+        },
+      ],
+    };
+  },
+};
